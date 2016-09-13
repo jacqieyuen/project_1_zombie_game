e5e4972d36f3b31aad04281fe07f56dfb4122669
Jacquelyn Yuen <yuencd@gmail.com>
2016-09-13 12:35:48 +0800

    organized, functions ready, need to add pop up instructions/Play and end game Highscore board/ reset game 'done' button

--- game.js
+++ game.js
@@ -17,50 +17,50 @@ document.onmousemove = function(mouse){
 
   player.x = mouseX;
   player.y = mouseY;
-}
+  }
 testCollision = function (yay1,yay2){
-    var rect1 = {
-    x: yay1.x-((yay1.width)/2),
-    y: yay1.y-((yay1.height)/2),
-    width: yay1.width,
-    height: yay1.height,
-    };
-    var rect2 = {
-    x:yay2.x-((yay2.width)/2),
-    y: yay2.y-((yay2.height)/2),
-    width:yay2.width,
-    height:yay2.height,
-    };
-    return collisionRect(rect1,rect2);
-}
+  var rect1 = {
+  x: yay1.x-((yay1.width)/2),
+  y: yay1.y-((yay1.height)/2),
+  width: yay1.width,
+  height: yay1.height,
+  };
+  var rect2 = {
+  x:yay2.x-((yay2.width)/2),
+  y: yay2.y-((yay2.height)/2),
+  width:yay2.width,
+  height:yay2.height,
+  };
+  return collisionRect(rect1,rect2);
+  }
 //collision function for rectangles
 collisionRect= function(rect1,rect2){
-   return (rect1.x < rect2.x + rect2.width &&
-   rect1.x + rect1.width > rect2.x &&
-   rect1.y < rect2.y + rect2.height &&
-   rect1.height + rect1.y > rect2.y);
-}
+  return (rect1.x < rect2.x + rect2.width &&
+  rect1.x + rect1.width > rect2.x &&
+  rect1.y < rect2.y + rect2.height &&
+  rect1.height + rect1.y > rect2.y);
+  }
 //function to draw rectangles
 draw_something = function(something){
   ctx.save(); // make sure to save so that we can revert the color back to normal
   ctx.fillStyle = something.color;
   ctx.fillRect(something.x-((something.width)/2), something.y-((something.width)/2),something.width,something.height); // dimensions of rectangle
   ctx.restore(); // restore ctx to original settings
-}
+  }
 // functions for zombies
 update_z = function(something){
   update_z_position(something);
   draw_something(something);
-}
+  }
 //function to update position
 update_z_position = function (something) {
   something.y += something.spdY;
-}
+  }
 gameEnd = function(){
   player.color = "red";
   console.log("YOU HAVE DIED! You have a score of "+ score +"!")
   clearInterval(Interval);
-}
+  }
 gameInit = function(){ // initiate game
   Interval = setInterval(update, 30); // SET INTERVAL
   function update(){
@@ -91,48 +91,9 @@ gameInit = function(){ // initiate game
     draw_something(player);
     ctx.fillText(("Player Life: " + player.lives),500,550);
     ctx.fillText(("Score: " + score),500,500);
+  };
   }
-}
-gameInit();
 
+//initialize game. Use Play Button to Initiate.
+// gameInit();
 
-
-// var Game = function(){
-
-//   var score = 0;
-//   var zombies = [];
-//   var frame = 0;
-
-
-
-
-//   var render = function(){
-
-//     for (var i = zombies.length - 1; i >= 0; i--) {
-//       zombies[i].render();
-//     }
-
-
-
-//   }
-
-
-//   window.requestAnimFrame = (function(){
-//     return  window.requestAnimationFrame       ||
-//             window.webkitRequestAnimationFrame ||
-//             window.mozRequestAnimationFrame    ||
-//             function( callback ){
-//               window.setTimeout(callback, 1000 / 60);
-//             };
-//   })();
-
-
-//   (function animloop(){
-//     requestAnimFrame(animloop);
-//     render();
-//   })();
-
-// }
-
-
-// var g = new Game();
